363953bd7d004b3b1367e408d0f5a87136d668f6
Bassam <bassam.gamal.lam3y@gmail.com>
2024-08-16 15:16:12 +0300

    Changes to be committed: 	modified:   python/main.py

--- python/main.py
+++ python/main.py
@@ -1,12 +1,25 @@
 import json
 import tkinter as tk
 
-with open(
-    "/home/duck/Downloads/theme maker/settings.json", mode="r", encoding="utf-8") as read_file:
+with open("/home/duck/Downloads/theme maker/settings.json") as read_file:
     all_sittings = json.load(read_file)
 
 window = tk.Tk()
 window.title("VS Code theme maker")
+
+window_width = 300
+window_height = 200
+
+# get the screen dimension
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+# calculate the center point
+center_x = int(screen_width / 2 - window_width / 2)
+center_y = int(screen_height / 2 - window_height / 2)
+
+# set the position of the window to the center of the screen
+window.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
 
 message = tk.Label(window, text="test")
 message.pack()
