665f213c74fcdbded1f2c8f5a38e37826f5375ff
Bassam <bassam.gamal.lam3y@gmail.com>
2024-08-16 15:05:35 +0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   python/main.py

--- python/main.py
+++ python/main.py
@@ -1,14 +1,14 @@
 import json
 import tkinter as tk
 
-window = tk.Tk()
-window.title("VS Code theme maker")
-window.mainloop()
-
-with open("/home/duck/Downloads/theme maker/settings.json", mode="r", encoding="utf-8") as read_file:
+with open(
+    "/home/duck/Downloads/theme maker/settings.json", mode="r", encoding="utf-8") as read_file:
     all_sittings = json.load(read_file)
 
+window = tk.Tk()
+window.title("VS Code theme maker")
 
+message = tk.Label(window, text="test")
+message.pack()
 
-for i in all_sittings["workbench.colorCustomizations"]:
-    print(i)
+window.mainloop()
